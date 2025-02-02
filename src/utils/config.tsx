c2c18d6f7c23c147265033a6f38b2c1c2c47f319
Dmitriy Barbashin <dmitriy.barbashin@inlinegroup-c.ru>
2025-02-02 13:19:21 +0300

    # config

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -3,8 +3,12 @@
 //     port: ':5000'
 // }
 const data = {
-    host: 'localhost',
-    port: ':5000'
+    // host: 'localhost',
+    // port: ':5000',
+    host: '78.107.239.94',
+    port: ':80'
 }
+
+
 
 export default data;
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 80,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     }
+// }
+// })
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 80,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://78.107.239.94:80',
         changeOrigin: true,
         secure: false,
       },
     }
 }
 })
-
